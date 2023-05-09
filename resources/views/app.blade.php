343f26bdebe5df2de6b1066b519ad2e925c5ef2b
“Hardik <hardik.patel@acquaintsoft.com>
2023-05-09 15:42:22 +0530

    override a package

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="shortcut icon" href="{{ asset('/vendor/mixpost/favicon.ico') }}">
+    {{-- testing  --}}
     @routes
     {{ mixpostAssets() }}
     @inertiaHead
